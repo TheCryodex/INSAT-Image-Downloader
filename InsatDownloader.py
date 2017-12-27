9ede284a083d4e9f883a61c642b89acb8ebebabc
TheCryodex <sarjakchawda@gmail.com>
2017-12-27 11:09:45 +0530

    Add files via upload

---
+++ InsatDownloader.py
@@ -0,0 +1,20 @@
+import urllib.request
+import schedule
+import time
+import datetime
+
+now=datetime.datetime.now()
+
+
+def downloadimage(url):
+    now = datetime.datetime.now()
+    tname=now.strftime("%Y-%m-%d %H%M")
+    filename=str(tname) +".jpg"
+    urllib.request.urlretrieve(url,filename)
+
+downloadimage("http://satellite.imd.gov.in/img/3Dglobe_ir1.jpg")
+schedule.every(30).minutes.do(downloadimage,"http://satellite.imd.gov.in/img/3Dglobe_ir1.jpg")
+
+while(1):
+    schedule.run_pending()
+    time.sleep(1)
